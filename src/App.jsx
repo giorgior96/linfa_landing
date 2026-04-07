40f9f188e83ddaf1c99e36167bc201ff06feaea8
Giorgio <giorgio.giorgior@gmail.com>
2026-04-07 10:14:17 +0200

    Optimize Lenis smoothing config for premium scrolling experience

--- src/App.jsx
+++ src/App.jsx
@@ -970,9 +970,10 @@ import Lenis from 'lenis';
 function App() {
   useEffect(() => {
     const lenis = new Lenis({
-      lerp: 0.08,
+      duration: 1.6,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      wheelMultiplier: 1,
+      wheelMultiplier: 1.1,
     });
     let rafId;
 
